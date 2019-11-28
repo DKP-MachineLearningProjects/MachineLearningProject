3ec97455d2951e029fb21ef8205665793c97b18f
dheerajkedar <dheerajkedar@gmail.com>
2019-11-28 18:03:05 -0500

    decision tree classifier added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from Plot import plot_confusion_matrix
 import matplotlib.pyplot as plt
@@ -7,9 +8,15 @@ from LoadSplit import LoadData
 from LoadSplit import DataSplit
 
 def SVM_train_predict(X_train, X_test, y_train):
-    svclassifier1 = SVC(kernel='poly')
-    svclassifier1.fit(X_train, y_train)
-    y_pred = svclassifier1.predict(X_test)
+    svclassifier = SVC(kernel='linear')
+    svclassifier.fit(X_train, y_train)
+    y_pred = svclassifier.predict(X_test)
+    return y_pred
+
+def DecisionTree_train_predict(X_train, X_test, y_train):
+    DTC=DecisionTreeClassifier()
+    DTC.fit(X_train, y_train)
+    y_pred = DTC.predict(X_test)
     return y_pred
 
 
@@ -20,18 +27,24 @@ def main():
     X_train, X_test, y_train, y_test = DataSplit(X,y)
     
     y_pred=SVM_train_predict(X_train, X_test, y_train)
-    
     np.set_printoptions(precision=2)
+    plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True, title='Normalized confusion matrix SVM')
 
-    # Plot non-normalized confusion matrix
-    plot_confusion_matrix(y_test, y_pred, classes=class_names,title='Confusion matrix, without normalization')
-
-    #   Plot normalized confusion matrix
-    plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True, title='Normalized confusion matrix')
-
+    y_pred=DecisionTree_train_predict(X_train, X_test, y_train)
+    np.set_printoptions(precision=2)
+    plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True, title='Normalized confusion matrix for DT')
     plt.show()
 
-    #print(classification_report(y_test,y_pred))
+
+    # # # Plot non-normalized confusion matrix
+    # # plot_confusion_matrix(y_test, y_pred, classes=class_names,title='Confusion matrix, without normalization')
+
+    # #   Plot normalized confusion matrix
+    # plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True, title='Normalized confusion matrix')
+
+    # plt.show()
+
+    # #print(classification_report(y_test,y_pred))
     
 
 main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+# Plot non-normalized confusion matrix
+    # plot_confusion_matrix(y_test, y_pred, classes=class_names,title='Confusion matrix, without normalization')
